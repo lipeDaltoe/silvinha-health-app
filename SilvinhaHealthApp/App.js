f8f22e7c7f474b130232037b5c1c81ad963e8a62
lipeDaltoe <felipedaltoe9@gmail.com>
2024-02-26 12:27:03 -0300

    Criado o projeto e o componente title

--- SilvinhaHealthApp/App.js
+++ SilvinhaHealthApp/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/Title';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Title />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#e0e5e5',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title:{
+    color: '#FF0043',
+    fontSize: 28,
+    fontWeight: "bold"
+  }
 });
---
+++ SilvinhaHealthApp/src/components/Title/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+import { styles } from './style';
+
+export default function Title() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Silvinha Health App</Text>
+    </View>
+  );
+}
