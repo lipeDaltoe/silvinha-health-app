be57088c234b202866a85c1fa867b7be54f4c43e
lipeDaltoe <felipedaltoe9@gmail.com>
2024-03-11 12:18:15 -0300

    Próxima aula: Cálculo IMC

--- SilvinhaHealthApp/src/components/Form/style.js
+++ SilvinhaHealthApp/src/components/Form/style.js
@@ -1,16 +1,50 @@
 import { StyleSheet } from "react-native"
 
 const styles = StyleSheet.create({
-    formContext: {
-        width: '100%',
-        height:'100%',
-        bottom: 0,
-        backgroundColor: "#FFFFFF",
-        alignItems: 'center',
-        marginTop: 30,
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-    },
-})
+  formContext: {
+    width: '100%',
+    height: '100%',
+    bottom: 0,
+    backgroundColor: "#FFFFFF",
+    alignItems: 'center',
+    marginTop: 30,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  formGroup: {
+    width: "100%",
+    height: "auto",
+    marginTop: 30,
+    padding: 10,
+  },
+  formLabel: {
+    color: "#000000",
+    fontSize: 18,
+    paddingLeft: 20,
+  },
+  input: {
+    width: "90%",
+    height: 40,
+    fontSize: 18,
+    borderRadius: 20,
+    backgroundColor: "#f6f6f6",
+    margin: 12,
+    paddingHorizontal: 15,
+  },
+  button: {
+    backgroundColor: "#FF0043",
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    width: '90%',
+    paddingVertical: 14,
+    marginLeft: 12,
+    margin: 30,
+  },
+  textButton: {
+    fontSize: 22,
+    color: "#FFFFFF",
+  },
+});
 
 export default styles;
